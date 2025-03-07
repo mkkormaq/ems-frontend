84d2bd472dbb45f2522668bfd7b88cd3c6c3a8d1
Josh McCormack <joshgmc@gmail.com>
2025-03-06 20:26:40 -0500

    added header and footer components and routes for list employee component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "./App.css";
+import FooterComponent from "./components/FooterComponent";
+import HeaderComponent from "./components/HeaderComponent";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 /**
  * The main App component.
@@ -10,7 +13,14 @@ import ListEmployeeComponent from "./components/ListEmployeeComponent";
 function App() {
   return (
     <>
-      <ListEmployeeComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<ListEmployeeComponent />}></Route>
+          <Route path="/employees" element={<ListEmployeeComponent />}></Route>
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   );
 }
